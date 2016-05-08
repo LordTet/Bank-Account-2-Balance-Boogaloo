ddc722b10d910ce4a0ae84107c584f1152103242
LordTet <holthamj@gmail.com>
2016-05-08 15:21:12 -0400

    Fixed stuff

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -3,18 +3,23 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.EmptyTransition;
 import org.newdawn.slick.Color;
 public class MainMenu extends BasicGameState
 {
+	private StateBasedGame game;
 	public void init(GameContainer container, StateBasedGame game) throws SlickException 
     {
-        // TODO Auto-generated method stub
+        this.game = game;
     }
 
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException
     {
         g.setColor(Color.red);
-        g.drawString("Ayy lmao",50,10);
+        g.drawString("A rediculously bad title screen for Proof of Concept Purposes!",100,50);
+        g.setColor(Color.white);
+        g.drawString("1. Play game",100,150);
+        g.drawString("2. Exit",100,200);
 
     }
  
@@ -27,4 +32,15 @@ public class MainMenu extends BasicGameState
 	{
         return 0;
 	}
+    
+    public void keyReleased(int key, char c)
+    {
+    	System.out.println(key);
+    	switch(key)
+    	{
+    	case 2:
+    		game.enterState(1, new EmptyTransition(), new EmptyTransition());
+    		break;
+    	}
+    }
 }
