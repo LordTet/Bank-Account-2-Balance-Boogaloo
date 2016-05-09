5e9436eb72cf041d40fa5a79c867560d67b6c550
LordTet <holthamj@gmail.com>
2016-05-08 20:21:59 -0400

    added .class to gitignore, makes it easier to sync,

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,6 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 import java.util.Scanner;
-import java.io.File;
 public class Test extends StateBasedGame
 {
 
